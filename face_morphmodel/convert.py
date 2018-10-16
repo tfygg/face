5daf7832d7f140507ecf9f61b2884de5ce555496
chaiyujin <chaiyujin@gmail.com>
2018-10-16 17:34:04 +0800

    ...

--- face_morphmodel/convert.py
+++ face_morphmodel/convert.py
@@ -37,8 +37,8 @@ class H5Util:
             fp.write(struct.pack('i', attr.ndim))
             for idim in range(attr.ndim):
                 fp.write(struct.pack('i', attr.shape[idim]))
-            print("dim: {}, shape: {}".format(attr.ndim, attr.shape[:attr.ndim]))
             attr.tofile(fp)
+            # print("dim: {}, shape: {}".format(attr.ndim, attr.shape[:attr.ndim]))
         with open(path, "wb") as fp:
             bin_write(fp, data["mean"])
             bin_write(fp, data["pcaBasis"])
@@ -57,14 +57,34 @@ class H5Util:
         self.expression = self._read_group("/expression/")
         self._write(self.expression, os.path.join(output_dir, "expression"))
 
+    def _export_landmark(self, output_dir):
+        lms = self.file["/metadata/landmarks/text"].value.decode("utf-8").split("\n")
+        while len(lms[-1]) == 0:
+            lms = lms[:-1]
+        with open(os.path.join(output_dir, "landmarks.txt"), "w") as fp:
+            fp.write("\n".join(lms))
+    
+    def _export_representer(self, output_dir):
+        def bin_write(fp, attr):
+            fp.write(struct.pack('i', attr.ndim))
+            for idim in range(attr.ndim):
+                fp.write(struct.pack('i', attr.shape[idim]))
+            attr.tofile(fp)
+            # print("dim: {}, shape: {}".format(attr.ndim, attr.shape[:attr.ndim]))
+        with open(os.path.join(output_dir, "template.bin"), "wb") as fp:
+            bin_write(fp, self.file["/shape/representer/cells"].value)
+            bin_write(fp, self.file["/shape/representer/points"].value)
+
     def export(self, output_dir):
-        os.makedirs(output_dir, exist_ok=True)
-        self._export_color(output_dir)
-        self._export_shape(output_dir)
-        self._export_expression(output_dir)
+        # os.makedirs(output_dir, exist_ok=True)
+        # self._export_color(output_dir)
+        # self._export_shape(output_dir)
+        # self._export_expression(output_dir)
+        self._export_landmark(output_dir)
+        self._export_representer(output_dir)
 
 
-for k in model_paths:
-    model_name = model_paths[k]
-    util = H5Util(os.path.join(h5_path, model_name))
-    util.export("../assets/basel/{}/".format(k))
+face_util = H5Util(os.path.join(h5_path, model_paths["face"]))
+head_util = H5Util(os.path.join(h5_path, model_paths["head"]))
+face_util.export("../assets/basel/{}/".format("face"))
+head_util.export("../assets/basel/{}/".format("head"))
